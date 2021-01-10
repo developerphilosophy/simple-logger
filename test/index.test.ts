72595c9ac285c2f1e23840f8b9f8695a383190ee
lovebhardwaj <lovebhardwaj@gmail.com>
2021-01-09 19:19:21 -0500

    MODIFIED: Tests to add delay function

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,40 +1,43 @@
-import { expect } from "chai";
-import SimpleLogger from "../src/index";
-import * as path from "path";
-import * as fs from "fs";
+import { expect } from 'chai';
+import SimpleLogger from '../src/index';
+import * as path from 'path';
+import * as fs from 'fs';
 
-describe("UNIT: Test import and initialization", () => {
-	it("Should create a logs file if it does not exists", async function () {
-		return new Promise(async (resolve, reject) => {
-			try {
-				await SimpleLogger.debug("Test");
-				const logDirPath = path.join(process.env.PWD as string, "logs");
-				expect(fs.existsSync(logDirPath)).to.be.true;
-				return resolve();
-			} catch (error) {
-				return reject(error);
-			}
-		});
-	});
+describe('UNIT: Test import and initialization', () => {
+  const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-	it("Should write a debug output to the logs", async () => {
-		const result = await SimpleLogger.debug("Debug output goes here");
-		expect(result).to.be.true;
-	});
+  it('Should create a logs file if it does not exists', async function () {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await SimpleLogger.debug('Test');
+        const logDirPath = path.join(process.env.PWD as string, 'logs');
+        await delay(2000);
+        expect(fs.existsSync(logDirPath)).to.be.true;
+        return resolve();
+      } catch (error) {
+        return reject(error);
+      }
+    });
+  });
 
-	it("Should write a info message to logs", async () => {
-		const result = await SimpleLogger.info("Info message goes here");
-		expect(result).to.be.true;
-	});
+  it('Should write a debug output to the logs', async () => {
+    const result = await SimpleLogger.debug('Debug output goes here');
+    expect(result).to.be.true;
+  });
 
-	it("Should write a warn message to logs", async () => {
-		const result = await SimpleLogger.info("Warn message goes here");
-		expect(result).to.be.true;
-	});
+  it('Should write a info message to logs', async () => {
+    const result = await SimpleLogger.info('Info message goes here');
+    expect(result).to.be.true;
+  });
 
-	it("Should write error stack to logs and return error key", async () => {
-		const testError = new Error("This is a test error message");
-		const errorKey = await SimpleLogger.error(testError);
-		expect(errorKey).to.be.greaterThan(0);
-	});
+  it('Should write a warn message to logs', async () => {
+    const result = await SimpleLogger.info('Warn message goes here');
+    expect(result).to.be.true;
+  });
+
+  it('Should write error stack to logs and return error key', async () => {
+    const testError = new Error('This is a test error message');
+    const errorKey = await SimpleLogger.error(testError);
+    expect(errorKey).to.be.greaterThan(0);
+  });
 });
